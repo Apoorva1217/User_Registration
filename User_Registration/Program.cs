8010df6a2f8fa25d75de156c7c25276935789ba1
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-26 22:46:01 +0530

    [Apoorva] Add . Added code for valid first name, first name starts with Cap and has minimum 3 characters

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework.Internal;
+using System;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
 
 namespace User_Registration
 {
@@ -7,6 +10,16 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program!");
+            Console.WriteLine("Enter First Name:");
+            string firstName = Console.ReadLine();
+            if(!Regex.Match(firstName, "^[A-Z][a-z]{2,}$" ).Success)
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Valid First Name");
+            }
         }
     }
 }
