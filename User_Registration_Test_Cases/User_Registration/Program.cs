11ce05fb6a370334c2d13914608ad1e0a18d42be
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-28 21:33:00 +0530

    [Apoorva] Add . Added Program.cs and Patterns.cs file for User Registration

---
+++ User_Registration_Test_Cases/User_Registration/Program.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+
+namespace User_Registration
+{
+    class Program
+    {
+        /// <summary>
+        /// User Registration Program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program!");
+        }
+    }
+}
