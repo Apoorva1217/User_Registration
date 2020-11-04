22d01e15343c84364a07d737fd11a02ef52be3af
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-04 13:48:08 +0530

    [Apoorva] Add. Welcome to User Registration Program

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -1,7 +1,4 @@
-﻿using NUnit.Framework.Internal;
-using System;
-using System.Runtime.Versioning;
-using System.Text.RegularExpressions;
+﻿using System;
 
 namespace User_Registration
 {
@@ -14,68 +11,6 @@ namespace User_Registration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program!");
-
-            //Validate First Name using Regex Pattern
-            Console.WriteLine("Enter First Name:");
-            string firstName = Console.ReadLine();
-            if(!Regex.Match(firstName, "^[A-Z][a-z]{2,}$" ).Success)
-            {
-                Console.WriteLine("Invalid First Name \n");
-            }
-            else
-            {
-                Console.WriteLine("Valid First Name \n");
-            }
-
-            //Validate Last Name using Regex Pattern
-            Console.WriteLine("Enter Last Name:");
-            string lastName = Console.ReadLine();
-            if (!Regex.Match(lastName, "^[A-Z][a-z]{2,}$" ).Success)
-            {
-                Console.WriteLine("Invalid Last Name \n");
-            }
-            else
-            {
-                Console.WriteLine("Valid Last Name \n");
-            }
-
-            //Validate Email ID using Regex Pattern
-            Console.WriteLine("Enter Email ID:");
-            string emailID = Console.ReadLine();
-            if (!Regex.Match(emailID, "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+[.]+([a-zA-Z]{2,4})+[.]*([a-zA-Z]{2})*$" ).Success)
-            {
-                Console.WriteLine("Invalid Email ID \n");
-            }
-            else
-            {
-                Console.WriteLine("Valid Email ID \n");
-            }
-
-            //Validate Mobile Number using Regex Pattern
-            Console.WriteLine("Enter Mobile Number:");
-            string mobileNumber = Console.ReadLine();
-            if (!Regex.Match(mobileNumber, "^[0-9]{2} [0-9]{10}" ).Success)
-            {
-                Console.WriteLine("Invalid Mobile Number \n");
-            }
-            else
-            {
-                Console.WriteLine("Valid Mobile Number \n");
-            }
-
-            //Validate Password using Regex Pattern
-            Console.WriteLine("Enter Password:");
-            string password = Console.ReadLine();
-
-            //Rule 4-> has exactly one special character
-            if (!Regex.Match(password, "(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*-+]{1}).{8,}" ).Success)
-            {
-                Console.WriteLine("Invalid Password \n");
-            }
-            else
-            {
-                Console.WriteLine("Valid Password \n");
-            }
         }
     }
 }
