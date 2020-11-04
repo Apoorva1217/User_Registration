719b4fb8fcbcdd6147dac935f32ae033a5990f77
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-04 20:48:36 +0530

    [Apoorva] Refactor . Refactored the code to use Lambda function to validate user entry

--- User_Registration/push.sh
+++ User_Registration/push.sh
@@ -2,7 +2,7 @@ read a
 git branch $a
 git checkout $a
 git add .
-git commit -m "[Apoorva] Add . Unit Test to validate user entry"
+git commit -m "[Apoorva] Refactor . Refactored the code to use Lambda function to validate user entry"
 git push origin $a
 git checkout master
 git merge $a
