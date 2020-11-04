9706814ac986b26dc0166aa85de09dcfa871c12f
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-04 20:51:17 +0530

    Delete push.sh

--- User_Registration/push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Refactor . Refactored the code to use Lambda function to validate user entry"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
