194ce2b6112fdf123dc9224c1e6dfb6ecf418dcf
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-04 16:57:50 +0530

    Delete push.sh

--- User_Registration/push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Refactor . Refactored the code to throw exception in case of invalid user details"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
